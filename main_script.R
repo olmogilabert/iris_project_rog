fb3651c5da5e4cc9385fdc4fddf5f7cc6b1722bc
Olmo-Gilabert.R <olmo.gilabert@gmail.com>
2024-02-06 11:49:52 +0100

    added setosa subsetting

--- main_script.R
+++ main_script.R
@@ -1,3 +1,11 @@
 iris_data <- iris
 library(dplyr)
 library(ggplot2)
+library(readr)
+
+# reading data
+iris_data <- read_csv('raw_data/iris.csv')
+
+# subsetting data
+setosa_subset <- iris_data |>
+  filter(Species == "setosa")
